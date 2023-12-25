a0e3255d303c73eeb52a434f7dc0ea34173aa9c9
TasiYokan <guanghao.yang@outlook.com>
2023-12-25 16:42:44 +0800

    Fix python binding issue for TNearPointLocator

--- CDT/extras/VerifyTopology.h
+++ CDT/extras/VerifyTopology.h
@@ -28,7 +28,7 @@ namespace CDT
  * @tparam T type of vertex coordinates (e.g., float, double)
  * @tparam TNearPointLocator class providing locating near point for efficiently
  */
-template <typename T, typename TNearPointLocator>
+template <typename T, typename TNearPointLocator = LocatorKDTree<T>>
 inline bool verifyTopology(const CDT::Triangulation<T, TNearPointLocator>& cdt)
 {
     // Check if vertices' adjacent triangles contain vertex
